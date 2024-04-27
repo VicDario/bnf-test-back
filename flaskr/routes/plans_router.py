30311fd3e736c826350970cd7aa49ef3f558d79d
VicDario <26585356+VicDario@users.noreply.github.com>
2024-04-27 14:06:31 -0400

    feat: plans route

--- flaskr/routes/plans_router.py
+++ flaskr/routes/plans_router.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
+from flaskr.repository.plans_repository import PlansRepository
 
 plans = Blueprint('plans', __name__)
 
 @plans.route('/')
 def get_plans():
-    return 'Plans information'
+    plans = PlansRepository.get_all_with_features()
+    return plans
